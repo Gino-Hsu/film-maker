b1496dbaf70df0f66b43425171c3d1f5c246150d
GinoHsu <ginohsu16@gmail.com>
2023-03-24 22:19:16 +0800

    feat: 新增 header, 完成滾動顯示/隱藏

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,28 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+
+import Header from '../components/Header';
+
 import styles from '@/styles/Home.module.scss';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    // window.scrollY 垂直滾動的 pix，用 setState 存起來
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    // 掛載監聽器，監聽 window 的滾動事件
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,7 +31,19 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={styles.main}></main>
+      <main>
+        <div
+          className={
+            // 當垂直滾動超過300px時，header隱藏起來
+            scrollPosition > 300
+              ? styles.headerDisplay__hidden
+              : styles.headerDisplay
+          }
+        >
+          <Header />
+        </div>
+        <section className={styles.cards}>122</section>
+      </main>
     </>
   );
 }
